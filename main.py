95c24778f0a613b1f26f32c5d4392d34da683f1b
Alvin Cho <79716732+acho-fiserv@users.noreply.github.com>
2023-03-31 10:47:09 +0000

    clean up

--- main.py
+++ main.py
@@ -7,23 +7,27 @@ from strawberry.asgi import GraphQL
 #class SearchInput:
 #    search_string: str
 
-       
 @strawberry.type
 class SearchResult:
     score: float
     title: str
     snippet: str
     link: str
+    
+@strawberry.type
+class SearchResults:
+    results: typing.List[SearchResult]
+    
 
-def search(self, search_input: str) -> SearchResult:
-    if search_input == "Getting Started":
-        return SearchResult(score=1.0, title="Getting Started", snippet="hi snippet data", link="/doc/getting-started.md")
+def search(self, input: str) -> typing.List[SearchResult]:
+    if input == "Getting Started":
+        return [SearchResult(score=1.0, title="Getting Started", snippet="hi snippet data", link="/doc/getting-started.md"), SearchResult(score=1.0, title="Apple Pay", snippet="apple pay", link="/doc/apple-pay.md")]
     else:
-        return SearchResult(score=1.0, title="Apple Pay", snippet="apple pay", link="/doc/apple-pay.md")
+        return [SearchResult(score=1.0, title="Apple Pay", snippet="apple pay", link="/doc/apple-pay.md")]
     
 @strawberry.type
 class Query:
-    results: SearchResult = strawberry.field(resolver=search)
+    search: typing.List[SearchResult] = strawberry.field(resolver=search)
 
     
 
